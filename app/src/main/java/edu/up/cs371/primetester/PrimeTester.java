5f6e5edbced47d0c96cbe781c9e6ad7e82447f41
NalaniKai <megan.nalani@gmail.com>
2015-09-02 16:27:41 -0700

    Attempt number one

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -14,7 +14,25 @@ public class PrimeTester {
      * @return true iff n is prime
      */
     public static boolean isPrime(long n) {
+        boolean answer = false;
+
+        if(n == 0 || n == 1){
+            //answer is false
+        }
+        else if(n == 2){
+            answer = true;
+        }
+        //check if odd or divisible by and odd number
+        else if(n%2 > 0){
+            for(int i = 3; i <= 1999; i = i+2){
+                if(n%i > 0){
+                    answer = true;
+                }
+            }
+        }
+
+        return answer;
         // for now, return a random result
-        return Math.random() > 0.5;
+        //return Math.random() > 0.5;
     }
 }
